0deffb67538a228f24101941901ded4002d45e80
Misato <rexlove1226@gmail.com>
2024-07-02 08:43:21 -0700

    add news section

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import News from "../components/main_page/News";
+
+export default function Home() {
+  return (
+    <>
+      <h1>News</h1>
+      <News />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import News from "./components/main_page/News";
+import Link from "next/link";
 
-export default function Home() {
-  return <News />;
+export default function Page() {
+  return <Link href="/home">Go to home</Link>;
 }
