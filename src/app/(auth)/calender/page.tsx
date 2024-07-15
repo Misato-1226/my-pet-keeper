2ff803fc05d49e78f3da7fa144b40cade5200826
Misato <rexlove1226@gmail.com>
2024-07-14 23:18:03 -0600

    make calender section / footer

--- src/app/(auth)/calender/page.tsx
+++ src/app/(auth)/calender/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // needed for dayClick
+import Modal from "@/app/components/calendar/Modal";
 
 export default function Calendar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const events = [
     {
       id: "a", // ユニークID
@@ -19,19 +21,33 @@ export default function Calendar() {
     // 省略
   ];
 
-  const handleDateClick = useCallback((arg: DateClickArg) => {
-    alert(arg.dateStr);
-  }, []);
+  const handleCustomButtonClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="p-12">
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        selectable={true}
-        editable={true}
-        dateClick={handleDateClick}
-      />
-    </div>
+    <>
+      <div className="p-12">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          events={events}
+          editable={true}
+          customButtons={{
+            customButton: {
+              text: "Add Event",
+              click: handleCustomButtonClick,
+            },
+          }}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "customButton",
+          }}
+        />
+      </div>
+
+      {isModalOpen && <Modal onClose={handleCustomButtonClick} />}
+    </>
   );
 }
