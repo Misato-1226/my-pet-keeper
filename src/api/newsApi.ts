90a223b4f0c863e097570bc9c6cd0d8c5c9ae39b
Misato <rexlove1226@gmail.com>
2024-07-24 16:05:14 +0900

    fix news api to get latest news

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -1,12 +1,37 @@
 import axios from "axios";
 const apiKey = process.env.NEWS_APIKEY;
 
+const today = new Date();
+
+const getToday = () => {
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  return `${year}-${month.toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}`;
+};
+
+const getYesterday = () => {
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
+  const yearYesterday = yesterday.getFullYear();
+  const monthYesterday = yesterday.getMonth() + 1;
+  const dayYesterday = yesterday.getDate();
+
+  return `${yearYesterday}-${monthYesterday
+    .toString()
+    .padStart(2, "0")}-${dayYesterday.toString().padStart(2, "0")}`;
+};
+
 export const getNews = async () => {
   try {
     const response = await axios.get(
       //can't use apikey from .env
-      `https://newsapi.org/v2/everything?q=pet AND dogs&q=pet AND cats&pageSize=6&apiKey=beb5b2cc41034b718e22d2c71b8a7ad2`
+      `https://newsapi.org/v2/everything?q=pets AND dogs&q=pets AND cats&from=${getYesterday()}&to=${getToday()}&pageSize=6&apiKey=beb5b2cc41034b718e22d2c71b8a7ad2`
     );
+
     if (response.status !== 200) {
       throw new Error("Network response was not ok");
     }
