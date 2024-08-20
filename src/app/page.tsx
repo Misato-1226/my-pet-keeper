af22eaa3751f5d938464ce61e699dc6f0956478e
Misato <rexlove1226@gmail.com>
2024-08-21 00:26:27 +0900

    fix page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,5 @@ import Weather from "./components/main_page/Weather";
 import UpcomingTask from "./components/main_page/UpcomingEvent";
 
 export default function Page() {
-  return (
-    <>
-      <Link href="/home">Go to home</Link>
-      <div className="flex flex-col gap-y-20">
-        <PetPreviewBar />
-        <Weather />
-        <News />
-        <UpcomingTask />
-      </div>
-    </>
-  );
+  return <></>;
 }
