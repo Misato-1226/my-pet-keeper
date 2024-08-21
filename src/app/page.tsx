a8adbe435c94e1dcfa6a5bf02abfb38d72bd81e0
Misato <rexlove1226@gmail.com>
2024-08-21 21:43:23 +0900

    change page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
+import { redirect } from "next/navigation";
 
-import Link from "next/link";
-import News from "./components/main_page/News";
-import PetPreviewBar from "./components/main_page/PetPreviewBar";
-import Weather from "./components/main_page/Weather";
-import UpcomingTask from "./components/main_page/UpcomingEvent";
-
-export default function Page() {
-  return <></>;
+export default function page() {
+  redirect("/sign-in");
 }
